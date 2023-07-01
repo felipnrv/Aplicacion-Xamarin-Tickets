7759074ebe331a1c9fb3ec8f43ddcee3059a8acc
Felipe Narvaez <felipe.98np@outlook.com>
2023-07-01 00:22:13 -0500

    Add project files.

---
+++ XamarinGrupo3/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinGrupo3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecuperarContrasena_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciarSesion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistro_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
