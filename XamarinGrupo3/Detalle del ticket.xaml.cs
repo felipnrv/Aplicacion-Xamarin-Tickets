d3166d6f267444fcf8dd38d6b9fab5e66b9a1e9d
Felipe Narvaez <felipe.98np@outlook.com>
2023-07-03 10:05:31 -0500

    Vista de los tickets

---
+++ XamarinGrupo3/Detalle del ticket.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinGrupo3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Detalle_del_ticket : ContentPage
+    {
+        public Detalle_del_ticket()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinGrupo3/DetallesTicket.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinGrupo3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetallesTicket : ContentPage
+    {
+        public DetallesTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNuevoTicket_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TicketsListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+    }
+}
