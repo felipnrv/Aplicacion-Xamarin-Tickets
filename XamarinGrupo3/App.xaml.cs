a8c47380adce3af5fc9c9e7f273237b271bf8627
Estefrank07 <estebanmoya46@gmail.com>
2023-07-13 20:57:23 -0500

    arreglo login

--- XamarinGrupo3/App.xaml.cs
+++ XamarinGrupo3/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamarinGrupo3
             InitializeComponent();
 
             //Navegar entre paginas
-            MainPage = new NavigationPage(new Registro());
+            MainPage = new NavigationPage(new Login("",""));
         }
 
         protected override void OnStart()
